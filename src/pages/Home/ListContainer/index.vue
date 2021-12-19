9a388d945e0830eae4cbb1dc074d66d31e3e702d
WeCcRy <76674193+WeCcRy@users.noreply.github.com>
2021-12-19 16:44:58 +0800

    Add files via upload

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -5,18 +5,9 @@
         <!--banner轮播-->
         <div class="swiper-container" id="mySwiper">
           <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <img src="./images/banner1.jpg" />
+            <div class="swiper-slide" v-for="carousel in listContainer" :key="carousel.id">
+              <img :src="carousel.imgUrl" />
             </div>
-<!--            <div class="swiper-slide">-->
-<!--              <img src="./images/banner2.jpg" />-->
-<!--            </div>-->
-<!--            <div class="swiper-slide">-->
-<!--              <img src="./images/banner3.jpg" />-->
-<!--            </div>-->
-<!--            <div class="swiper-slide">-->
-<!--              <img src="./images/banner4.jpg" />-->
-<!--            </div>-->
           </div>
           <!-- 如果需要分页器 -->
           <div class="swiper-pagination"></div>
@@ -111,6 +102,7 @@
 
 <script>
 import {mapState} from "vuex";
+import Swiper from 'swiper';
 export default {
   name: "ListContainer",
   mounted() {
@@ -122,8 +114,29 @@ export default {
         return state.home.listContainer
       }
     })
+  },
+  watch:{
+    listContainer(){
+      this.$nextTick(()=>{
+        var mySwiper = new Swiper ('.swiper-container', {
+          loop: true, // 循环模式选项
+
+          // 如果需要分页器
+          pagination: {
+            el: '.swiper-pagination',
+          },
+
+          // 如果需要前进后退按钮
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          }
+        })
+      })
+    }
+    }
   }
-}
+
 </script>
 
 <style scoped>
