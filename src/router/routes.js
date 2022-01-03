33f21bcd6f779f5d28bca1a7e7fdfd6072beb4d3
WeCcRy <76674193+WeCcRy@users.noreply.github.com>
2022-01-03 15:51:41 +0800

    Add files via upload

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,8 @@ import Detail from "../pages/Detail";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Search from "../pages/Search";
+import AddCartSuccess from '../pages/AddCartSuccess'
+import ShopCart from '../pages/ShopCart'
 
 export default [
     {
@@ -32,6 +34,17 @@ export default [
         name:'search',
         component:Search,
         meta:{show:true}
+    },
+    {
+      path:'/addcartsuccess',
+        name:'addcartsuccess',
+      component: AddCartSuccess,
+        meta: {show: true}
+    },
+    {
+        path:'/shopcart',
+        component: ShopCart,
+        meta: {show: true}
     },
     //路由重定向
     {
