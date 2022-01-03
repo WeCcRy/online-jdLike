c211fcde649e3105b22d31db4567a385d40101f4
WeCcRy <76674193+WeCcRy@users.noreply.github.com>
2022-01-03 15:51:15 +0800

    Add files via upload

--- src/api/index.js
+++ src/api/index.js
@@ -8,3 +8,9 @@ export const requestListContainer=()=>MockRequest({url:'/banner',method:'get'})
 export const requestFloorList=()=>MockRequest({url:'/floor',method:'get'})
 
 export const requestGetSearch=(params)=>requests({url:'/list',method:'post',data:params})
+
+export const requsetDetail=(skuid)=>requests({url:`/item/${skuid}`,method:'get'})
+
+export const requestShopcart=(skuid,skuNum)=>requests({url:`/cart/addToCart/${skuid}/${skuNum}`,method:'post'})
+
+export const requestCartList=()=>requests({url:'/cart/cartList',method:'get'})
